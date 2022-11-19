f6334bd94ea4703cb07637b3efe2d7b8450a1e82
Faizan <fs8167@gmail.com>
2022-11-19 11:17:16 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Layout from "./components/layout"
+
+
+function AppRouter() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <>
+    <Route path="/" element={ <h1 className="font-bold underline">default</h1> } />
+    <Route path="/login" element={ <h1>login</h1>}/>
+    <Route path="/main" element={ <Layout/>}>
+    <Route index element={ <h1>hourly</h1>}/>
+    <Route index element={ <h1>daily</h1>}/>
+    <Route index element={ <h1>monthly</h1>}/>
+    </Route>
+    </>
+  ));
+
+  return <RouterProvider router={router}></RouterProvider>
+   
+  
+}
+
+export default function App(){
+return <AppRouter/>
+}
---
+++ src/components/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header'
+import { Outlet } from 'react-router-dom'
+
+export default function () {
+  return (
+    <>
+   <Header/>
+  <main>
+ <Outlet/>
+  </main>
+<footer></footer>
+    </>
+  )
+}
+
